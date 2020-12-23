e0bfaa265e39c4aa321d827d05fcbc758486c1dc
Dan Webb <mail@danwebb.co>
2020-12-23 11:25:07 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['poppins', 'sans-serif'],
+      serif: ['dm-serif-display', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
